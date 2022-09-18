9d8cf7bca5d922a4063dff61f8181674995ed7a3
Felip Miguel <fmiguel@microsoft.com>
2022-09-18 22:12:47 +0200

    JBoss EAP adapted

--- JakartaEE/jboss-eap/azure/deploy.sh
+++ JakartaEE/jboss-eap/azure/deploy.sh
@@ -4,10 +4,11 @@ MYSQL_HOST=mysql-jboss-passwordless
 DATABASE_NAME=checklist
 DATABASE_FQDN=${MYSQL_HOST}.mysql.database.azure.com
 # Note that the connection url includes the password-free authentication plugin
-MYSQL_CONNECTION_URL="jdbc:mysql://${DATABASE_FQDN}:3306/${DATABASE_NAME}?useSSL=true&requireSSL=true&defaultAuthenticationPlugin=com.azure.jdbc.msi.extension.mysql.AzureMySqlMSIAuthenticationPlugin&authenticationPlugins=com.azure.jdbc.msi.extension.mysql.AzureMySqlMSIAuthenticationPlugin"
+MYSQL_CONNECTION_URL="jdbc:mysql://${DATABASE_FQDN}:3306/${DATABASE_NAME}?useSSL=true&requireSSL=true&defaultAuthenticationPlugin=com.azure.identity.providers.mysql.AzureIdentityMysqlAuthenticationPlugin&authenticationPlugins=com.azure.identity.providers.mysql.AzureIdentityMysqlAuthenticationPlugin"
 APPSERVICE_NAME=jboss-passwordless
 APPSERVICE_PLAN=asp-jboss-passwordless
-LOCATION=eastus
+APP_IDENTITY_NAME=identity-jboss-passwordless
+LOCATION=westcentralus
 MYSQL_ADMIN_USER=azureuser
 # Generating a random password for the MySQL user as it is mandatory
 # mysql admin won't be used as Azure AD authentication is leveraged also for administering the database
@@ -24,7 +25,7 @@ CURRENT_USER_OBJECTID=$(az ad user show --id $CURRENT_USER --query id -o tsv)
 # create resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
-# create mysql server
+# create mysql flexible-server
 az mysql flexible-server create \
     --name $MYSQL_HOST \
     --resource-group $RESOURCE_GROUP \
@@ -44,6 +45,12 @@ az appservice plan create --name $APPSERVICE_PLAN --resource-group $RESOURCE_GRO
 # Create application service
 az webapp create --name $APPSERVICE_NAME --resource-group $RESOURCE_GROUP --plan $APPSERVICE_PLAN --runtime "JBOSSEAP:7-java8"
 
+# create managed identity for mysql. By assigning the identity to the mysql server, it will enable Azure AD authentication
+az identity create --name $APP_IDENTITY_NAME --resource-group $RESOURCE_GROUP --location $LOCATION
+
+IDENTITY_ID=$(az identity show --name $APP_IDENTITY_NAME --resource-group $RESOURCE_GROUP --query id -o tsv)
+IDENTITY_CLIENTID=$(az identity show --name $APP_IDENTITY_NAME --resource-group $RESOURCE_GROUP --query clientId -o tsv)
+
 # create service connection. 
 az webapp connection create mysql-flexible \
     --resource-group $RESOURCE_GROUP \
@@ -52,24 +59,24 @@ az webapp connection create mysql-flexible \
     --server $MYSQL_HOST \
     --database $DATABASE_NAME \
     --client-type java \
-    --system-identity 
+    --identity-resource-id $IDENTITY_ID \
+    --system-identity
 
-# POPULATE DATABASE
-# 0. Create a temporary firewall rule to allow connections from current machine to the mysql server
+# CREATE DATABASE SCHEMA
+# Create a temporary firewall rule to allow connections from current machine to the mysql server
 MY_IP=$(curl http://whatismyip.akamai.com)
-az mysql flexible-server firewall-rule create --resource-group $RESOURCE_GROUP --server $MYSQL_HOST --name AllowCurrentMachineToConnect --start-ip-address ${MY_IP} --end-ip-address ${MY_IP}
+az mysql flexible-server firewall-rule create --resource-group $RESOURCE_GROUP --name $MYSQL_HOST --rule-name AllowCurrentMachineToConnect --start-ip-address ${MY_IP} --end-ip-address ${MY_IP}
 
-# 1. Create Database tables
+# Create Database tables
 RDBMS_ACCESS_TOKEN=$(az account get-access-token --resource-type oss-rdbms --output tsv --query accessToken)
-mysql -h "${DATABASE_FQDN}" --user "${CURRENT_USER}@${MYSQL_HOST}" --enable-cleartext-plugin --password="$RDBMS_ACCESS_TOKEN" < init-db.sql
+mysql -h "${DATABASE_FQDN}" --user "${CURRENT_USER}" --enable-cleartext-plugin --password="$RDBMS_ACCESS_TOKEN" < init-db.sql
 
-# 2. Remove temporary firewall rule
-az mysql flexible-server firewall-rule delete --resource-group $RESOURCE_GROUP --server $MYSQL_HOST --name AllowCurrentMachineToConnect
+# Remove temporary firewall rule
+az mysql flexible-server firewall-rule delete --resource-group $RESOURCE_GROUP --name $MYSQL_HOST --rule-name AllowCurrentMachineToConnect
 
 # Build WAR file
 mvn clean package -DskipTests -f ../pom.xml
-# Set environment variables for the web application pointing to the database and using the appservice identity login
-# az webapp config appsettings set -g $RESOURCE_GROUP -n $APPSERVICE_NAME --settings MYSQL_CONNECTION_URL=${MYSQL_CONNECTION_URL} MYSQL_USER=${APPSERVICE_LOGIN_NAME}'@'${MYSQL_HOST}
-# Create webapp deployment
+
+# Deploy the WAR and the startup script to the app service
 az webapp deploy --resource-group $RESOURCE_GROUP --name $APPSERVICE_NAME --src-path ../target/ROOT.war --type war
 az webapp deploy --resource-group $RESOURCE_GROUP --name $APPSERVICE_NAME --src-path ../src/main/webapp/WEB-INF/createMySQLDataSource.sh --type startup
