1b4f0756f9d1bf0f7d9be3674c250e7e5d15feab
Felip Miguel <fmiguel@microsoft.com>
2022-09-18 22:27:02 +0200

    initial weblogic before adapting to latest sdk

---
+++ JakartaEE/weblogic/src/test/java/com/microsoft/azure/samples/SampleTest.java
@@ -0,0 +1,14 @@
+package com.microsoft.azure.samples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+  @Test
+  public void simpleJUnit5Test() {
+    String result = "duke";
+    assertEquals("duke", result);
+  }
+}
